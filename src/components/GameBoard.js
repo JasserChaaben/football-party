fe1a3d07658879f27107aaa042adf81d9812d208
JasserChaaben <chaabnjasser@gmail.com>
2024-12-06 20:15:44 +0100

    made it so that when the game Starts the button remove and everyone knows the game starts

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -4,9 +4,11 @@ import socket from '../socket';
 function GameBoard({players,setPlayers, lobbyId, playerName }) {
   const [error, setError] = useState(null);
   const [owner, setOwner] = useState(false);
+  const [gameStarted, setGameStarted] = useState(false);
   useEffect(() => {
     console.log("Joining lobby....");
     socket.emit('checkOwner',{lobbyId},({owner})=>{setOwner(owner);console.log("testing owner" + owner)})
+    socket.emit('gameStarted',{lobbyId},({started})=>{setGameStarted(started);console.log("testing game if started : " + started)})
    
     socket.off('updateLobby'); 
     socket.on('updateLobby', handleUpdateLobby); 
@@ -21,7 +23,8 @@ function GameBoard({players,setPlayers, lobbyId, playerName }) {
   const handleUpdateLobby = (players) => {
     console.log("test")
     setPlayers(players);
-    socket.emit('checkOwner',{lobbyId},({owner})=>{setOwner(owner);console.log("testing owner" + owner)})
+    socket.emit('checkOwner',{lobbyId},({owner})=>{setOwner(owner);console.log("testing owner : " + owner)})
+    socket.emit('gameStarted',{lobbyId},({started})=>{setGameStarted(started);console.log("testing game if started : " + started)})
     console.log('Players updated:', players);
   };
   return (
@@ -36,12 +39,11 @@ function GameBoard({players,setPlayers, lobbyId, playerName }) {
         ))}
       </ul>
       
-     {owner&& <input
+     {owner&& !gameStarted&&<input
   type="button"
   value="Start Game"
   onClick={() => {
     socket.emit('startGame', { lobbyId}, ({ result }) => {
-     alert(result);
     });
   }}
 />}
